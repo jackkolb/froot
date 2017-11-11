b77696c5a3beb61c30e4f510a72520471871fda7
Karen Kong <kkong006@ucr.edu>
2017-11-11 11:00:57 -0800

    Add javascript

---
+++ froot.js
@@ -0,0 +1,7 @@
+var recipeList = document.getElementsByTagName("LI");
+function loadElements() {
+    var li = document.createElement("li");
+    var t = document. createTextNode("New Recipe");
+    li.appendChild(t);
+    document.getElementById("recipeUL");
+}
